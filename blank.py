6bd590ef28ae541b1924b3fe287d7bf2d8abedc9
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2024-09-03 18:29:11 -0400

    Update blank.py

--- blank.py
+++ blank.py
@@ -2,4 +2,8 @@
 #Email:  ALEXANDER.LUSTIG23@myhunter.cuny.edu
 #Date: Septmeber 3, 2024
 #This program prints: Hello, World!
-print("Hello, World!")
+i = 0
+
+while i < 24:
+    print("Hello, World!")
+    i+=1
